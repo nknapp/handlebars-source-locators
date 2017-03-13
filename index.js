b9d5cb7912d044c32132cebd79329c5245849b70
Nils Knappmeier <github@knappi.org>
2017-03-13 11:29:33 +0100

    Support for source-locators that point to partials
    - "registerPartial" is wrapped such that a parsed AST of the partial
      is registered, where the "source"-flag is set on all nodes
    - source-locators are now always derived from the location-parameter
      of "appendToBuffer", because the srcName gets lost on the way to
      the line/column-properties at the Node
    - Add a source-locator befor **and after** each node (to account for
      the fewer times that "appendToBuffer" is called with a "location"
      parameter

--- index.js
+++ index.js
@@ -28,6 +28,22 @@ function addSourceLocators (handlebarsEnvironment) {
     return `<sl line="${sourceLine}" col="${sourceColumn}" partial="${partial}"></sl>`
   })
 
+  // Wrap "registerPartial":
+  // Register the parsed AST of the partial and make sure the the 'source'-property is set on all node
+  // so that we can pass the partial-name of to the source-locator
+  const originalRegisterPartial = handlebarsEnvironment.registerPartial.bind(handlebarsEnvironment)
+  handlebarsEnvironment.registerPartial = function registerPartialWithSourceLocators (name, value) {
+    console.log(name, value)
+    if (Object.prototype.toString.call(name) === '[object Object]') {
+      var partials = name
+      Object.keys(partials).forEach((name) => this.registerPartial(name, partials[name]))
+    } else {
+      // create AST
+      const parse = this.parse(value, { srcName: name })
+      // call wrapped function
+      originalRegisterPartial(name, parse)
+    }
+  }
   return handlebarsEnvironment
 }
 
@@ -37,11 +53,20 @@ class SourceMapCompiler extends Handlebars.JavaScriptCompiler {
     this.compiler = SourceMapCompiler
   }
 
+  formatSourceLocator (position, srcName) {
+    return srcName
+      ? `<sl line="${position.line}" col="${position.column}" partial="${srcName}"></sl>`
+      : `<sl line="${position.line}" col="${position.column}"></sl>`
+  }
+
   appendToBuffer (source, location, explicit) {
-    var result = super.appendToBuffer(source, location, true)
+    if (!location) {
+      return super.appendToBuffer(source, location, explicit)
+    }
     return [
-      super.appendToBuffer(`'<sl line="${result[1].line}" col="${result[1].column}">'`, location, true),
-      result
+      super.appendToBuffer(`'${this.formatSourceLocator(location.start, location.source)}'`, location, true),
+      super.appendToBuffer(source, location, true),
+      super.appendToBuffer(`'${this.formatSourceLocator(location.end, location.source)}'`, location, true)
     ]
   }
 }
--- test/main-spec.js
+++ test/main-spec.js
@@ -31,19 +31,92 @@ describe('handlebars-source-locators', function () {
   })
 
   it('should add source locators to output of the main template', function () {
-    const templateString = deindent`
+    const env = sourceLocators(Handlebars.create())
+
+    const template = env.compile(deindent`
       a{{a}}
       bb{{b}}
       ccc{{c}}d
-      `
-    const exppectedResult = deindent`
-      <sl line="1" col="0">a<sl line="1" col="1">1<sl line="1" col="6">
-      bb<sl line="2" col="2">2<sl line="2" col="7">
-      ccc<sl line="3" col="3">3<sl line="3" col="8">d
-      `
+      `)
+    expect(template({ a: 1, b: 2, c: 3 })).to.equal(deindent`
+      <sl line="1" col="0"></sl>a<sl line="1" col="1"></sl>1<sl line="1" col="6"></sl>
+      bb<sl line="2" col="2"></sl>2<sl line="2" col="7"></sl>
+      ccc<sl line="3" col="3"></sl>3<sl line="3" col="8"></sl>d<sl line="3" col="9"></sl>
+      `)
+  })
+
+  it('should include the partial-name, if the source is located in a partial', function () {
     const env = sourceLocators(Handlebars.create())
-    const output = env.compile(templateString)({ a: 1, b: 2, c: 3 })
-    expect(output).to.equal(exppectedResult)
+
+    var template = env.compile(deindent`
+      partial-output:
+      ----
+      {{> myPartial}}
+      
+      ----
+      
+      `)
+
+    env.registerPartial('myPartial', deindent`
+      a{{a}}
+      bb{{b}}
+      ccc{{c}}d
+      
+      `)
+
+    expect(template({ a: 1, b: 2, c: 3 })).to.equal(deindent`
+      <sl line="1" col="0"></sl>partial-output:
+      ----
+      <sl line="3" col="0"></sl><sl line="1" col="0" partial="myPartial"></sl>a<sl line="1" col="1" partial="myPartial"></sl>1<sl line="1" col="6" partial="myPartial"></sl>
+      bb<sl line="2" col="2" partial="myPartial"></sl>2<sl line="2" col="7" partial="myPartial"></sl>
+      ccc<sl line="3" col="3" partial="myPartial"></sl>3<sl line="3" col="8" partial="myPartial"></sl>d
+      <sl line="4" col="0" partial="myPartial"></sl><sl line="3" col="15"></sl>
+      ----
+      <sl line="6" col="0"></sl>
+      `)
+  })
+
+  it('should work with multiple partial-registration', function () {
+    const env = sourceLocators(Handlebars.create())
+
+    var template = env.compile(deindent`
+      partial-output:
+      ----
+      {{> partial1}}
+      
+      ----
+      {{> partial2}}
+      
+      ----
+      
+      `)
+
+    env.registerPartial({
+      partial1: deindent`
+        a{{a}}
+        bb{{b}}
+        
+        `,
+      partial2: deindent`
+        c{{c}}
+        dd{{c}}
+        
+        `
+    })
+
+    expect(template({a: 1, b: 2, c: 3, d: 4})).to.equal(deindent`
+      <sl line="1" col="0"></sl>partial-output:
+      ----
+      <sl line="3" col="0"></sl><sl line="1" col="0" partial="partial1"></sl>a<sl line="1" col="1" partial="partial1"></sl>1<sl line="1" col="6" partial="partial1"></sl>
+      bb<sl line="2" col="2" partial="partial1"></sl>2<sl line="2" col="7" partial="partial1"></sl>
+      <sl line="3" col="0" partial="partial1"></sl><sl line="3" col="14"></sl>
+      ----
+      <sl line="6" col="0"></sl><sl line="1" col="0" partial="partial2"></sl>c<sl line="1" col="1" partial="partial2"></sl>3<sl line="1" col="6" partial="partial2"></sl>
+      dd<sl line="2" col="2" partial="partial2"></sl>3<sl line="2" col="7" partial="partial2"></sl>
+      <sl line="3" col="0" partial="partial2"></sl><sl line="6" col="14"></sl>
+      ----
+      <sl line="9" col="0"></sl>
+      `)
   })
 })
 
