b10fda5aed72a973155cc256732044dbd0910ca9
Nils Knappmeier <github@knappi.org>
2017-03-13 10:02:19 +0100

    Source-Locators for template contents

--- index.js
+++ index.js
@@ -4,14 +4,44 @@
  * Copyright (c) 2017 Nils Knappmeier.
  * Released under the MIT license.
  */
+var Handlebars = require('handlebars')
 
-'use strict'
-
-module.exports = handlebarsSourceLocators
+module.exports = addSourceLocators
 /**
- * Describe your module here
+ * Adds source-locators so a Handlebars instance.
+ * This is rejected, if Handlebars is the original Handlebars-environment.
+ * Please use "Handlebars.create" to create a new environment and
+ * pass that to this function.
+ *
+ * @param {Handlebars} a Handlebars environment
+ *
+ *
  * @public
  */
-function handlebarsSourceLocators () {
-  // body
+function addSourceLocators (handlebarsEnvironment) {
+  if (handlebarsEnvironment === Handlebars) {
+    throw new Error('Refusing to apply source-locators to the default Handlebars environment. Please use "Handlebars.create()"')
+  }
+
+  handlebarsEnvironment.JavaScriptCompiler = SourceMapCompiler
+  handlebarsEnvironment.registerHelper('createSourceLocator', function (sourceLine, sourceColumn, partial) {
+    return `<sl line="${sourceLine}" col="${sourceColumn}" partial="${partial}"></sl>`
+  })
+
+  return handlebarsEnvironment
+}
+
+class SourceMapCompiler extends Handlebars.JavaScriptCompiler {
+  constructor () {
+    super()
+    this.compiler = SourceMapCompiler
+  }
+
+  appendToBuffer (source, location, explicit) {
+    var result = super.appendToBuffer(source, location, true)
+    return [
+      super.appendToBuffer(`'<sl line="${result[1].line}" col="${result[1].column}">'`, location, true),
+      result
+    ]
+  }
 }
--- test/main-spec.js
+++ test/main-spec.js
@@ -5,15 +5,70 @@
  * Released under the MIT license.
  */
 
-/* global describe */
-// /* global it */
-// /* global xdescribe */
-// /* global xit */
+/* eslint-env mocha */
 
-'use strict'
+const sourceLocators = require('../')
+const Handlebars = require('handlebars')
+const chai = require('chai')
+const expect = chai.expect
 
-var handlebarsSourceLocators = require('../')
+describe('handlebars-source-locators', function () {
+  it('should reject the original Handlebars environment', function () {
+    expect(function () {
+      sourceLocators(Handlebars)
+    }).to.throw(Error, 'Refusing to apply source-locators to the default Handlebars environment.')
+  })
 
-describe('handlebars-source-locators:', function () {
-  // body
+  it('should not reject an environment created by Handlebars.create() ', function () {
+    expect(function () {
+      sourceLocators(Handlebars.create())
+    }).not.to.throw()
+  })
+
+  it('should return the modified environment', function () {
+    const handlebarsEnvironment = Handlebars.create()
+    expect(sourceLocators(handlebarsEnvironment)).to.equal(handlebarsEnvironment)
+  })
+
+  it('should add source locators to output of the main template', function () {
+    const templateString = deindent`
+      a{{a}}
+      bb{{b}}
+      ccc{{c}}d
+      `
+    const exppectedResult = deindent`
+      <sl line="1" col="0">a<sl line="1" col="1">1<sl line="1" col="6">
+      bb<sl line="2" col="2">2<sl line="2" col="7">
+      ccc<sl line="3" col="3">3<sl line="3" col="8">d
+      `
+    const env = sourceLocators(Handlebars.create())
+    const output = env.compile(templateString)({ a: 1, b: 2, c: 3 })
+    expect(output).to.equal(exppectedResult)
+  })
 })
+
+/**
+ * Remove indents and the first and last line from a multiline string
+ * The last line defines the amount of indentation to be removed.
+ * The first line is always expected to be empty
+ * The list line is expected to consist only of spaces
+ * @param strings
+ * @param values
+ */
+function deindent (strings, ...values) {
+  const result = String.raw.apply(String, arguments)
+  const lines = result.split('\n')
+  // Remote last and first line (both should be empty)
+  const lastLine = lines.pop()
+  if (!lastLine.match(/ +/)) {
+    throw new Error('Last line is expected to consist only of spaces (at least one)')
+    // ... or de-indentation is not needed
+  }
+  const firstLine = lines.shift()
+  if (firstLine !== '') {
+    throw new Error('First line is expected to be empty')
+  }
+  // The last line defines the mount of de-indentation
+  return lines.map((line) => line.substr(lastLine.length))
+    .join('\n')
+}
